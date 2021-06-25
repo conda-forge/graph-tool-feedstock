44c63c28a28d2ba19b1e61977f0cfcfb99feb112
Stuart Berg <bergs@janelia.hhmi.org>
2021-06-24 22:46:20 -0400

    Move import tests into a script where faulthandler can be enabled

---
+++ recipe/test_all_imports.py
@@ -0,0 +1,7 @@
+import faulthandler
+faulthandler.enable()
+
+import graph_tool
+import graph_tool.all
+import graph_tool.draw
+import graph_tool.draw.cairo_draw
---
+++ recipe/test_base_imports.py
@@ -0,0 +1,7 @@
+import faulthandler
+faulthandler.enable()
+
+import graph_tool
+
+# Will emit a warning due to drawing not being available, but that's fine
+import graph_tool.all
